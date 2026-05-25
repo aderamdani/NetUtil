3b5ca1f09332f572d2dacd311a8ad721cd0430be
Ade Ramdani <mr.aderamdani@gmail.com>
2026-05-26 04:55:26 +0700

    Initial Commit

---
+++ NetUtil/NetUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetUtilApp.swift
+//  NetUtil
+//
+//  Created by Ade Ramdani on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NetUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
