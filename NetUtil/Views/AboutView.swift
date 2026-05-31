bbcd8cb1db32466c253581be25a99609951045ac
Ade Ramdani <mr.aderamdani@gmail.com>
2026-05-31 17:38:02 +0700

    fix: resolve compiler timeout in Exporter.swift and release v4.4.1

--- NetUtil/Views/AboutView.swift
+++ NetUtil/Views/AboutView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct AboutView: View {
-    private let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "4.4.0"
+    private let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "4.4.1"
     private var updater = Updater.shared
 
     var body: some View {
