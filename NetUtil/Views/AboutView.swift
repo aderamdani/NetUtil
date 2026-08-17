b644853cf3bb0a949675543b525439060137e73e
Ade Ramdani <mr.aderamdani@gmail.com>
2026-08-18 06:20:10 +0900

    chore: bump version to v4.11.1

--- NetUtil/Views/AboutView.swift
+++ NetUtil/Views/AboutView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct AboutView: View {
-    private let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "4.11.0"
+    private let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "4.11.1"
     private var updater = Updater.shared
 
     var body: some View {
